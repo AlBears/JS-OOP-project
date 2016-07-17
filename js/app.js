1991fd75e69079b409873b1fc029605a40dc62ff
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-07-17 18:59:18 +0300

    Transform functions into object, multiplying

--- js/app.js
+++ js/app.js
@@ -1,16 +1,27 @@
 function onReady() {
 
-	setInterval(updateClock, 1000);
-	updateClock();
-
+	var clock = createClock('clock');
+	var clock2 = createClock('clock2');
 }
 
-function updateClock() {
-	var date = new Date();
+function createClock(id) {
+	var c = {};
+	c.updateClock = function() {
+		var date = new Date();
 
-	var clock = document.getElementById('clock');
-	clock.innerHTML = formatDigits(date.getHours()) + ':' + formatDigits(date.getMinutes()) + ':' + formatDigits(date.getSeconds());
+		var clock = document.getElementById(id);
+		clock.innerHTML = formatDigits(date.getHours()) + ':' + 
+		formatDigits(date.getMinutes()) + ':' + 
+		formatDigits(date.getSeconds());
+	};
+
+	setInterval(c.updateClock, 1000);
+	c.updateClock();
+
+	return c;
 }
+
+
 
 function formatDigits(val) {
 	if (val < 10) val = "0" + val;
