0708e98361571996f92da1600366f1a3a87c1bfa
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-07-17 19:22:12 +0300

    Add THIS, keep formating object

--- js/app.js
+++ js/app.js
@@ -10,22 +10,20 @@ function createClock(id) {
 		var date = new Date();
 
 		var clock = document.getElementById(id);
-		clock.innerHTML = formatDigits(date.getHours()) + ':' + 
-		formatDigits(date.getMinutes()) + ':' + 
-		formatDigits(date.getSeconds());
+		clock.innerHTML = this.formatDigits(date.getHours()) + ':' + 
+		this.formatDigits(date.getMinutes()) + ':' + 
+		this.formatDigits(date.getSeconds());
 	};
 
-	setInterval(c.updateClock, 1000);
+	c.formatDigits = function(val) {
+	if (val < 10) val = "0" + val;
+	return val;
+	};
+
+	setInterval(function(){c.updateClock();}, 1000);
 	c.updateClock();
 
 	return c;
-}
-
-
-
-function formatDigits(val) {
-	if (val < 10) val = "0" + val;
-	return val;
 }
 
 window.onload = onReady;
