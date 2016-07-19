b19d08d017e9f95d8e78e08c954d49b467207eaf
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-07-19 18:55:19 +0300

    Add prototype, taking methods out of scope

--- js/app.js
+++ js/app.js
@@ -3,33 +3,38 @@ function onReady() {
 	var clock = new Clock('clock', 180);
 	var clock2 = new Clock('clock2', 0, 'GMT');
 	var clock3 = new Clock('clock3', -240, 'NY');
+
 }
 
 function Clock(id, offset, label) {
 	label = label || '';
 	offset = offset || 0;
 	var d = new Date();
-	this.offset = (offset + d.getTimezoneOffset())*60*1000;
-
-	this.updateClock = function() {
-		var date = new Date();
-			date = new Date(this.offset + date.getTime());
-
-		var clock = document.getElementById(id);
-		clock.innerHTML = this.formatDigits(date.getHours()) + ':' + 
-		this.formatDigits(date.getMinutes()) + ':' + 
-		this.formatDigits(date.getSeconds()) +' '+ label;
-	};
-
-	this.formatDigits = function(val) {
-	if (val < 10) val = "0" + val;
-	return val;
-	};
+	this.id = id;
+	this.label = label;
+	this.offset = (offset + d.getTimezoneOffset()) * 60 * 1000;
 
 	var that = this;
-	setInterval(function(){that.updateClock();}, 1000);
+	setInterval(function() {
+		that.updateClock();
+	}, 1000);
 	this.updateClock();
 
 }
+
+Clock.prototype.updateClock = function() {
+	var date = new Date();
+	date = new Date(this.offset + date.getTime());
+
+	var clock = document.getElementById(this.id);
+	clock.innerHTML = this.formatDigits(date.getHours()) + ':' +
+		this.formatDigits(date.getMinutes()) + ':' +
+		this.formatDigits(date.getSeconds()) + ' ' + this.label;
+};
+
+Clock.prototype.formatDigits = function(val) {
+	if (val < 10) val = "0" + val;
+	return val;
+};
 
 window.onload = onReady;
